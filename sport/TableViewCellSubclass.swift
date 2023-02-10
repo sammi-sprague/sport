0b18a8188fd8479ca9e002018d7ef49a1002d71b
Sammi Sprague <samantha.sprague23@d155.org>
2023-02-10 07:34:26 -0600

    hi

---
+++ sport/TableViewCellSubclass.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewCellSubclass.swift
+//  sport
+//
+//  Created by SAMANTHA SPRAGUE on 2/9/23.
+//
+
+import UIKit
+
+class TableViewCellSubclass: UITableViewCell {
+    
+    
+    
+    @IBOutlet weak var typeOutlet: UILabel!
+    @IBOutlet weak var dateOutlet: UILabel!
+    @IBOutlet weak var locationOutlet: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configure(e: Events){
+        typeOutlet.text = 
+    }
+
+}
