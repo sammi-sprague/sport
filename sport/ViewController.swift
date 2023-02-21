cfbbdff12704f0016a0456a9007997434ffe0db4
Alex <alexander.carlson24@D155.org>
2023-02-21 07:50:28 -0600

    salami is stupid

--- sport/ViewController.swift
+++ sport/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AppData{
-    static var events = [Events(date: "Feb 2, 4:30", type: "Game", here: true, opp: "CLS", loc: "CLC")]
+    static var events = [Events(date: "Feb 2, 4:30", type: "Game", here: true, opp: "CLS", loc: "CLC", scoreCLC: 0, scoreOpp: 0)]
     static var selected = events[0]
 }
 
@@ -31,12 +31,21 @@ class Events: Codable{
     var here: Bool
     var opp: String
     var loc: String
+    var scoreCLC: Int
+    var scoreOpp: Int
     
-    init(date: String, type: String, here: Bool, opp: String, loc: String){
-        self.date = date
+    init(date: String, type: String, here: Bool, opp: String, loc: String, scoreCLC: Int, scoreOpp: Int){
+        self.date = "date"
         self.type = type
         self.here = here
         self.opp = opp
         self.loc = loc
+        self.scoreCLC = 0
+        self.scoreOpp = 0
+    }
+    
+    func setScores(scoreHome: Int, scoreOpp: Int){
+        self.scoreCLC = scoreHome
+        self.scoreOpp = scoreOpp
     }
 }
